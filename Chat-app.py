41668bf195e73590b053c1c616d2697605a2c5f1
Marlin <74386482+MarlinZH@users.noreply.github.com>
2025-06-26 23:37:18 -0400

    Update Chat-app.py

--- Chat-app.py
+++ Chat-app.py
@@ -5,6 +5,7 @@ from azure.identity import DefaultAzureCredential
 from azure.ai.projects import AIProjectClient
 from azure.ai.projects.models import FunctionTool, ToolSet
 from user_functions import user_functions --Data to feed to the Agent
+#TODO: Sync differences
 
 
 def main(): 
