d07a7368ef915bd2572ee71d0e6d6bddb4831dbf
Marlin <74386482+MarlinZH@users.noreply.github.com>
2026-01-11 23:56:06 -0500

    Update agent-flow.py

--- agent-flow.py
+++ agent-flow.py
@@ -1,2 +1,5 @@
 import pandas
-import microsoft
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+from azure.ai.projects.models import FunctionTool, ToolSet
+from user_functions import user_functions
